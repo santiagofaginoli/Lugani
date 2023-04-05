ed67530207666f018e600e06c0df41974de1ee0d
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-04-04 22:45:14 -0300

    Add files via upload

---
+++ components/Form.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import {
+  Card,
+  Col,
+  Row,
+  Button,
+  Text,
+  Container,
+  Grid,
+  Spacer,
+  Input,
+  Textarea,
+} from "@nextui-org/react";
+
+export default class Form extends Component {
+  render() {
+    return (
+      <Grid.Container alignItems="center" justify="center">
+        <form >
+          <Grid.Container >
+            <Grid.Container justify="center">
+              <Grid justify="center" xs={4}>
+                <Input
+                  css={{ h: "100%", w: "100%" }}
+                  placeholder="Nombre"
+                  type="text"
+                  id="nombre"
+                  name="nombre"
+                />
+              </Grid>
+              
+              <Spacer></Spacer>
+              <Grid xs={4}>
+                <Input
+                  css={{ h: "100%", w: "100%" }}
+                  placeholder="Email"
+                  type="text"
+                  id="email"
+                  name="email"
+                />
+              </Grid>
+            </Grid.Container>
+            <Spacer y={1}></Spacer>
+            <Grid.Container justify="center">
+              <Grid xs={7}>
+                <Textarea
+                  placeholder="Contenido"
+                  id="mensaje"
+                  name="mensaje"
+                  type="text"
+                  minRows={5}
+                  maxRows={10}
+                  css={{ h: "100%", w: "100%" }}
+                />
+              </Grid>
+            </Grid.Container>
+            <Spacer y={1}></Spacer>
+            <Grid.Container>
+              <Grid xs={12} justify="center">
+                <Button type="submit" className="boton-form">
+                  Enviar
+                </Button>
+              </Grid>
+            </Grid.Container>
+          </Grid.Container>
+        </form>
+      </Grid.Container>
+    );
+  }
+}
