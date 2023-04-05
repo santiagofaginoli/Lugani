345ddedf4e92513e5c2abfbc0846837d0628d444
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-04-04 22:44:47 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Navbar, Button, Link, Text, Grid } from "@nextui-org/react";
+import { Navbar, Button, Link, Text, Grid, Image } from "@nextui-org/react";
+
 
 export default function Layout({ children }) {
   return (
     <>
       <Grid.Container justify="center">
-        <Navbar className="navbar"  variant="floating">
+        <Navbar className="navbar" variant="floating">
           <Navbar.Content className="content-navbar" hideIn="xs" variant="highlight-rounded">
             <Navbar.Link className="items-navbar" href="#">Features</Navbar.Link>
-            <Navbar.Link  className="items-navbar" href="#">
+            <Navbar.Link className="items-navbar" href="#">
               Customers
             </Navbar.Link>
             <Navbar.Link className="items-navbar" href="#">Pricing</Navbar.Link>
@@ -17,6 +18,40 @@ export default function Layout({ children }) {
         </Navbar>
       </Grid.Container>
       {children}
+      <Grid.Container justify="center" style={{ marginTop: "80px", }} >
+        <Grid.Container xs={3} alignItems="center" style={{ backgroundColor: "white", borderRadius: "25px", height: "80px" }} justify="center">
+          <Grid xs={4}>
+            <Image
+              width={"70px"}
+              height={"70px"}
+              layout='intrinsic'
+              alt={'instagram'}
+              className="logo-ig"
+              src={"/img/instagram.svg"}>
+            </Image>
+          </Grid>
+          <Grid xs={4}>
+            <Image
+              width={"70px"}
+              height={"70px"}
+              layout='intrinsic'
+              alt={'instagram'}
+              className="logo-ig"
+              src={"/img/instagram.svg"}>
+            </Image>
+          </Grid>
+          <Grid xs={4}>
+            <Image
+              width={"70px"}
+              height={"70px"}
+              layout='intrinsic'
+              alt={'instagram'}
+              className="logo-ig"
+              src={"/img/instagram.svg"}>
+            </Image>
+          </Grid>
+        </Grid.Container>
+      </Grid.Container>
     </>
   );
 }
