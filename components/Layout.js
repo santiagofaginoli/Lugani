9ccc22a8ae6195609b6295e59fa291f17690d77b
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-04-13 19:49:35 -0300

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -5,15 +5,14 @@ import { Navbar, Button, Link, Text, Grid, Image } from "@nextui-org/react";
 export default function Layout({ children }) {
   return (
     <>
-      <Grid.Container justify="center">
+      <Grid.Container    justify="center">
         <Navbar className="navbar" variant="floating">
           <Navbar.Content className="content-navbar" hideIn="xs" variant="highlight-rounded">
-            <Navbar.Link className="items-navbar" href="#">Features</Navbar.Link>
-            <Navbar.Link className="items-navbar" href="#">
-              Customers
-            </Navbar.Link>
-            <Navbar.Link className="items-navbar" href="#">Pricing</Navbar.Link>
-            <Navbar.Link className="items-navbar" href="#">Company</Navbar.Link>
+            <Navbar.Link className="items-navbar" href="/">Home</Navbar.Link>
+            <Navbar.Link className="items-navbar" href="/contacto/contacto">Contacto</Navbar.Link>
+            <Navbar.Link className="items-navbar" href="/#About">About</Navbar.Link>
+            <Navbar.Link className="items-navbar" href="/#Project">Projects</Navbar.Link>
+            <Navbar.Link className="items-navbar" href="/#Skills">Skills</Navbar.Link>
           </Navbar.Content>
         </Navbar>
       </Grid.Container>
